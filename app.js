be88b0d86beaa2307ebd3007ef2be5a61ca832c5
Marlene <31359415+MNB-dev@users.noreply.github.com>
2021-11-06 20:08:02 -0300

    agregando chat

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const productosRouter = require('./routes/productos');
-const indexRouter = require('./routes/index');
-const hb = require('express-handlebars');
-const { Server: HttpServer } = require('http');
-const { Server: IOServer } = require('socket.io');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const productosRouter = require("./routes/productos");
+const indexRouter = require("./routes/index");
+const hb = require("express-handlebars");
+const { Server: HttpServer } = require("http");
+const { Server: IOServer } = require("socket.io");
 const PORT = 8080;
 const productos = [];
+const mensajes = [];
 
 const app = express();
 const httpServer = new HttpServer(app);
@@ -16,17 +17,20 @@ const io = new IOServer(httpServer);
 //--------------------------------------------
 // configuro el socket
 
-io.on('connection', async socket => {
-  //console.log('Nuevo cliente conectado!');
+io.on("connection", async (socket) => {
+  console.log('Nuevo cliente conectado!');
 
-  // carga inicial de productos
-  socket.emit('productos', productos);
+  socket.emit("productos", productos);
+  socket.on("update", (producto) => {
+    productos.push(producto);
+    io.sockets.emit("productos", productos);
+  });
 
-  // actualizacion de productos
-  socket.on('update', producto => {
-      productos.push(producto)
-      io.sockets.emit('productos', productos);
-  })
+  socket.emit("mensajes", mensajes);
+  socket.on("update-persona", (data) => {
+    mensajes.push(data);
+    io.sockets.emit("mensajes", mensajes);
+  });
 });
 
 //--------------------------------------------
@@ -34,42 +38,45 @@ io.on('connection', async socket => {
 app.engine(
   "hbs",
   hb({
-    extname:".hbs",
-    defaultLayout:"index.hbs",
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
     layoutsDir: __dirname + "/views/layout",
-    partialsDir:__dirname + "/views/partials/"
+    partialsDir: __dirname + "/views/partials/",
   })
 );
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static('public'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static("public"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/', indexRouter);
-app.use('/api', productosRouter);
+app.use("/", indexRouter);
+app.use("/api", productosRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
   res.json({ error: true, message: err.message });
 });
 
-
 const connectedServer = httpServer.listen(PORT, () => {
-  console.log(`Servidor http escuchando en el puerto ${connectedServer.address().port}`);
+  console.log(
+    `Servidor http escuchando en el puerto ${connectedServer.address().port}`
+  );
 });
-connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
+connectedServer.on("error", (error) =>
+  console.log(`Error en servidor ${error}`)
+);
 
 module.exports = app;
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,23 +1,60 @@
 const socket = io();
 
-const formAgregarProducto = document.getElementById('formAgregarProducto')
-formAgregarProducto.addEventListener('submit', e => {
-    e.preventDefault()
-    const producto = {
-        titulo: formAgregarProducto[0].value,
-        price: formAgregarProducto[1].value,
-        thumbnail:  formAgregarProducto[2].value
-    }
-    socket.emit('update', producto);
-    formAgregarProducto.reset()
-})
+const formAgregarProducto = document.getElementById("formAgregarProducto");
+formAgregarProducto.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const producto = {
+    titulo: formAgregarProducto[0].value,
+    price: formAgregarProducto[1].value,
+    thumbnail: formAgregarProducto[2].value,
+  };
+  socket.emit("update", producto);
+  formAgregarProducto.reset();
+});
 
-socket.on('productos', manejarEventoproductos);
+socket.on("productos", manejarEventoproductos);
 
 async function manejarEventoproductos(productos) {
-    const recursoRemoto = await fetch('plantillas/tabla-productos.hbs')
-    const textoPlantilla = await recursoRemoto.text()
-    const functionTemplate = Handlebars.compile(textoPlantilla)
-    const html = functionTemplate({ productos })
-    document.getElementById('productos').innerHTML = html
+  const recursoRemoto = await fetch("plantillas/tabla-productos.hbs");
+  const textoPlantilla = await recursoRemoto.text();
+  const functionTemplate = Handlebars.compile(textoPlantilla);
+  const html = functionTemplate({ productos });
+  document.getElementById("productos").innerHTML = html;
+}
+
+const formAgregarPersona = document.getElementById("formAgregarPersona");
+formAgregarPersona.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const p = {
+    mail: formAgregarPersona[0].value,
+    mensaje: formAgregarPersona[1].value,
+    hora: new Date(),
+  };
+  socket.emit("update-persona", p);
+  formAgregarPersona.reset();
+});
+
+socket.on("mensajes", manejarEventoPersona);
+
+async function manejarEventoPersona(msjs) {
+  const mensajesHTML = msjs
+    .map((msj) => {
+      const d = new Date(msj.hora);
+      const datestring =
+        d.getDate() +
+        "-" +
+        (d.getMonth() + 1) +
+        "-" +
+        d.getFullYear() +
+        " " +
+        d.getHours() +
+        ":" +
+        d.getMinutes() + 
+        ":" +
+        d.getSeconds();
+      return `${msj.mail} ${datestring} --- Mensaje: ${msj.mensaje}`;
+    })
+    .join("<br>");
+
+  document.getElementById("Persona").innerHTML = mensajesHTML;
 }
